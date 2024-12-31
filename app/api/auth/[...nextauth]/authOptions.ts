5bbe6455133bce576299cc4d9574e0f8288824d8
Dilitha ganegama <dilithacg05@gmail.com>
2024-12-31 21:13:54 +0530

    errorfixd

---
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -0,0 +1,26 @@
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    {
+      id: "descope",
+      name: "Descope",
+      type: "oauth",
+      wellKnown: `https://api.descope.com/P2ql9rZDt2Rfxwjx1W54jRSiRAid/.well-known/openid-configuration`,
+      authorization: { params: { scope: "openid email profile" } },
+      idToken: true,
+      clientId: "P2ql9rZDt2Rfxwjx1W54jRSiRAid",
+      clientSecret: process.env.DESCOPE_CLIENT_SECRET, // Replace with actual secret
+      checks: ["pkce", "state"],
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
+    },
+  ],
+};
+export default authOptions;
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,7 @@
 import NextAuth from "next-auth";
-import type { NextAuthOptions } from "next-auth";
+import authOptions from "./authOptions";
 
 // Define your NextAuth options
-export const authOptions: NextAuthOptions = {
-  providers: [
-    {
-      id: "descope",
-      name: "Descope",
-      type: "oauth",
-      wellKnown: `https://api.descope.com/P2ql9rZDt2Rfxwjx1W54jRSiRAid/.well-known/openid-configuration`,
-      authorization: { params: { scope: "openid email profile" } },
-      idToken: true,
-      clientId: "P2ql9rZDt2Rfxwjx1W54jRSiRAid",
-      clientSecret: process.env.DESCOPE_CLIENT_SECRET, // Replace with actual secret
-      checks: ["pkce", "state"],
-      profile(profile) {
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-        };
-      },
-    },
-  ],
-};
 
 // Export GET and POST methods for NextAuth
 const handler = NextAuth(authOptions);
